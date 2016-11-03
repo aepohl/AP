7b299965213498bb566e616f2db2973244183ab8
Andrew Pohl <aepohl@uark.edu>
2016-11-03 13:22:20 -0500

    add issue goods form

--- Inventory-management/issue_goods_form.cs
+++ Inventory-management/issue_goods_form.cs
@@ -10,16 +10,11 @@ using System.Windows.Forms;
 
 namespace Inventory_management
 {
-    public partial class Form1 : Form
+    public partial class issue_goods_form : Form
     {
-        public Form1()
+        public issue_goods_form()
         {
             InitializeComponent();
-        }
-
-        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
-
         }
     }
 }
