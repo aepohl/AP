a0a46c182257a67904ed26175fa630bf1815d141
Andrew Pohl <aepohl@uark.edu>
2016-11-03 13:28:59 -0500

    Create Product screen

---
+++ Inventory-management/Create_product_form.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Inventory_management
+{
+    public partial class Create_product_form : Form
+    {
+        public Create_product_form()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
